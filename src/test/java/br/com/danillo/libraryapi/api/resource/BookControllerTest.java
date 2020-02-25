9fbe2ca48e0783ced1d809714ffd50a44247ec79
Danillo <danillo.yamagush@gmail.com>
2020-02-25 00:30:30 -0300

    Inicio TDD do  projeto

--- src/test/java/br/com/danillo/libraryapi/api/resource/BookControllerTest.java
+++ src/test/java/br/com/danillo/libraryapi/api/resource/BookControllerTest.java
@@ -1,4 +1,44 @@
 package br.com.danillo.libraryapi.api.resource;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@WebMvcTest
+@AutoConfigureMockMvc
 public class BookControllerTest {
+
+    static String BOOK_API = "/api/books";
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    @DisplayName("Deve criar im livro com sucesso.")
+    public void createBookTest() throws Exception{
+
+        String json  = new ObjectMapper().writeValueAsString(null);
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .post(BOOK_API)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(json);
+    }
+
+    @Test
+    @DisplayName("Deve lancar um erro de validacao quando nao houver dados suficientes para criacao do livro.")
+    public void createIvalidBookTest(){}
 }
